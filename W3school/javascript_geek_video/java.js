6a3fb3f2c868c07abb8de95a618ca0dd1c25770d
thefullstackboy <deepaksewani03@gmail.com>
2021-04-17 09:48:50 +0530

    java

---
+++ W3school/javascript_geek_video/java.js
@@ -0,0 +1,181 @@
+
+//data type premettive
+//type of number
+//var a = 4;
+//document.write(typeof(a));
+//document.write("<br>");
+
+//type of string
+//var b ="5";
+//document.write(typeof(b));
+//document.write("<br>");
+ 
+//type of  object
+//var d=null;
+//document.write(typeof(d));
+//document.write("d");
+//document.write("<br>");
+
+
+//type of true/false boolean
+//var y = true;
+//document.write(typeof  (y));
+//document.write("<br>");
+
+
+//var h
+//document.write(typeof(h));
+//document.write("<br>");
+
+//arrery 
+//var brand =["iphone", "fsfsde", "Fer", "Rert"]
+//document.write(typeof(brand));
+//document.write("<br>");
+//alert("phone number" + brand[2])
+
+
+//object
+//var brand3 = {camera: "16mp", bettery:"1500mh", variant:"fef"}
+//document.write(typeof(brand3));
+//document.write("<br>");
+//alert("phone number  " + brand3.bettery)
+
+
+//input and output
+//var gfe = prompt("Enter you number");
+//document.write(gfe);
+//document.write("<br>");
+//document.writeln(gfe);
+//console.log("this"+gfe)
+
+//var g = 2;
+//var u =2;
+//var c =g+u; 
+//var d =g-u; 
+//var e =g/u; 
+//var f =g%u; 
+//document.write("Thid id additon "+ c) ;
+//document.write("<b>");
+//document.write("Thid id additon "+ d) ;
+//document.write("<b>");
+//document.write("Thid id additon "+ e) ;
+//document.write("<b>");
+//document.write("Thid id additon "+ f) ;
+
+//var b= 45;
+//var c=450;
+//var t= (b<c);
+//var e= (b>c);
+//var y= (b<=c);
+//var w= (b=>c);
+
+//document.write("This is  " +t);
+//document.write("<br>");
+//document.write("This is  " +e);
+//document.write("<br>");
+//document.write("This is  " +y);
+//document.write("<br>");
+//document.write("This is  " +w);
+//var a=45;
+//var b=45;
+//var c= (a | b);
+//document.write("Xor result "+ c);
+//document.write("<br>");
+
+//var d=(a^b);
+//document.write("Xor result "+ d);
+//document.write("<br>");
+
+//var e=(a<<b);
+//document.write("ls result "+ e);
+//document.write("<br>");
+
+//var f=(a<<b);
+//document.write("Xor result "+ f);
+//document.write("<br>");
+
+//var g=(a & b) ;
+//document.write("And result "+ g);
+//document.write("<br>");
+
+
+//assigment operators
+
+//var a=45;
+//var b=55;
+
+//c=a+b            _> c=a- b=b
+
+//var c= (a += b);
+//document.write("Result is " + c);
+//document.write("<br>");
+
+//var q= (a -= b);
+//document.write("Result is " + q);
+//document.write("<br>");
+
+//var c= (a /= b);
+//document.write("Result is " + c);
+//document.write("<br>");
+
+//var c= (a %= b);
+//document.write("Result is " + c);
+//document.write("<br>");
+
+//if else statement
+//var age = prompt("Enter your age");
+
+//if (age<18) {
+  //  document.write("you are not eligible for vote");
+//}
+
+//else if (age >= 18 && age <= 95){
+  //  document.write("you are not eligible for vote");
+//}
+
+//else if (age >= 96 && age <= 100){
+  //  document.write("you are Buzurg");
+//}
+
+//else {
+  //  document.write("you input invaild details")
+//}
+
+// document.write("press 1 for spelling");
+// document.write("<br>");
+// document.write("press 2 for spelling");
+// document.write("<br>");
+// document.write("press 3 for spelling");
+// document.write("<br>");
+// document.write("press 4 for spelling");
+// document.write("<br>");
+// document.write("press 5 for spelling");
+// document.write("<br>");
+
+// var usr=4;
+
+
+// switch (usr) {
+//     case 1:
+//         document.write("one");        
+//         break;
+
+//         case 2:
+//         document.write("two");        
+//         break;
+
+//         case 3:
+//         document.write("three");        
+//         break;
+
+//         case 4:
+//         document.write("four");        
+//         break;
+
+//         case 5:
+//         document.write("five");        
+//         break;
+// document.write("invalid input")
+//     default:
+        
+// }
