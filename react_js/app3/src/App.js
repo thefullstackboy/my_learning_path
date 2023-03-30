bfe9c1f19d36d662f49ed50c736b047f56e19967
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-03-30 13:25:09 +0530

    abc

---
+++ react_js/app3/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import About from './Componets/About';
+
+function App() {
+  return (
+    <div className="App">
+   <About/>    
+   <About/>    
+   <About/>    
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ react_js_practise/reactrevision/src/App.js
@@ -0,0 +1,12 @@
+import HookCounter from './componets/counter';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <HookCounter/>
+    
+    </div>
+  );
+}
+
+export default App;
